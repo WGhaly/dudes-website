a4b72756ae33542da18adde1f9066864bdb802b6
Waseem Ghaly <waseemghaly@example.com>
2026-01-20 00:04:25 +0200

    Add favicon using dudes logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "DUDES Advertising - Whatever It Is, We Do It",
   description: "With more than 15 years of experience in Marketing, Advertising and customer experience, we are eager to re-write your brand story and achieve tangible results together.",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
